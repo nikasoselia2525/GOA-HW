8a163db951d4a78e69e8fc228e4d7649411c33fe
nikasoselia2525 <nikasoselia827@gmail.com>
2024-06-08 22:44:32 +0400

    first commit

---
+++ day 01/homework/lesson2.py
@@ -0,0 +1,13 @@
+name ="nika"
+
+surname ="soselia"
+
+age = 15
+
+height = 170
+
+knows_programing = True
+
+is_short = False
+
+print("i'm " + name +" " + surname +". i'm" +" "+ str(age) + ". my hight is "+str(height)+"sm" )
