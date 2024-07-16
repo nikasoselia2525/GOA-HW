10f9c3ff3dbda6ef96353c98d51a35a6e08180ae
nikasoselia2525 <nikasoselia827@gmail.com>
2024-07-16 18:20:40 +0400

    commit

---
+++ day 11/classwork/level11.py
@@ -0,0 +1,8 @@
+name = input("Enter uour name: ")
+name2 = input("repit your name: ")
+
+print(name == name2)
+
+
+
+
---
+++ day 11/homework/level11.py
@@ -0,0 +1,10 @@
+gmail = input("Enter your Gmail: ")
+
+gmail2 = "nika@gmail.com"
+
+password = input("Enter your password: ")
+
+password2 = "nikanika"
+
+print(gmail == gmail2)
+print(password == password2)
