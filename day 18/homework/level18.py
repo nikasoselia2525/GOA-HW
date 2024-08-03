cf7e99d2c2c1184408ecbfe34662e022eadb61a8
nikasoselia2525 <nikasoselia827@gmail.com>
2024-08-03 17:25:57 +0400

    commit

---
+++ day 18/homework/level18.py
@@ -0,0 +1,38 @@
+# for i in range(0, 52, 2):
+#     print(i)
+
+
+# for i in range(1, 51, 2):
+#     print(i)
+
+
+# for i in range(10):
+#     print("goa")
+
+
+# for i in range(126, 0, -1):
+#     print(i)      
+
+
+# for i in range(126, 0, -2):
+#     print(i)      
+
+
+# for i in range(126, 0, -3):
+#     print(i)   
+
+
+# for i in range(30, 0, -3):
+#     print(i)
+
+
+# for i in range(30, 0, -2):
+#     print(i)
+
+
+# for i in range(0, 100, 5):
+#     print(i)
+
+
+for i in range(1, 67, 3):
+    print(i)
