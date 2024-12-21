7103d4a0239596fba26d9dc37019d2e1ae7a362d
nikasoselia2525 <nikasoselia827@gmail.com>
2024-12-21 23:02:57 +0400

    commit

---
+++ day 79/classwork/lvl79.js
@@ -0,0 +1,7 @@
+function showAlerts() {
+    alert("First Name: nika");
+    alert("Last Name: soselia");
+    alert("Age: 15 years");
+    alert("Favorite Car: tiger II (H)");
+    alert("Favorite Brand: Nike");
+}
